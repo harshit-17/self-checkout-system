e89e4ac51d814ad7d694212b37e315a3d942a30c
Bibekpreet Singh <bibekpreet99@gmail.com>
2021-08-28 11:44:20 +0530

    tweaks

--- self-checkout/App.js
+++ self-checkout/App.js
@@ -11,12 +11,7 @@ import { LogBox } from 'react-native'
 // import Register from './screens/Register'
 import authReducer from './centralstore/reducers/auth'
 import NavigationContainer from './navigation/navigationContainer'
-<<<<<<< HEAD
-import {adminReducer} from './centralstore/reducers/products'
-import BarcodeScanner from './components/BarcodeScanner'
-=======
 import { adminReducer } from './centralstore/reducers/products'
->>>>>>> 63e2662ee983b8ffabfd0675c1f0f91d9d65a546
 LogBox.ignoreAllLogs()
 const rootReducer = combineReducers({
   auth: authReducer,
@@ -30,13 +25,8 @@ export default function App() {
   return (
     <Provider store={store}>
       {/* <Register /> */}
-<<<<<<< HEAD
-      {/* <NavigationContainer /> */}
-      <BarcodeScanner/>
-=======
       <NavigationContainer />
 
->>>>>>> 63e2662ee983b8ffabfd0675c1f0f91d9d65a546
     </Provider>
   )
 
