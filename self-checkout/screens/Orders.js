fea8344932d2d39eb94c275e7f2fd3e37e6d86a7
Bibekpreet Singh <bibekpreet99@gmail.com>
2021-12-13 21:25:50 +0530

    user can now view previous orders

--- self-checkout/screens/Orders.js
+++ self-checkout/screens/Orders.js
@@ -1,22 +1,35 @@
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, View, Text, FlatList } from 'react-native';
+import { StyleSheet, View, Text, FlatList, Modal} from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 import axios from 'axios';
 import { apiEndPoint } from '../env/googleApi';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import ItemCard2 from '../components/ItemCard2';
+import ItemCard3 from '../components/ItemCard3';
+import { Entypo } from '@expo/vector-icons';
+
 
 const Orders = (props) => {
 
     const [orders, setOrders] = useState([])
-
+    const [modalVisible, setModalVisible] = useState(false)
+    const [modalList, setModalList] = useState([])
 
 
     const getData = async () => {
         let userData = await AsyncStorage.getItem("userData");
         let user = JSON.parse(userData).userId;
         let res = await axios.get(`${apiEndPoint}/users/${user}/orders.json`);
-
+        //console.log(res.data);
+        let list = []
+        let o = res.data
+        for(let i in o){
+            list.push(o[i]);
+        }
+        
+        list.reverse()
+        setOrders(list)
         // implement the conversion of object to array
         // setOrders(res.data)
     }
@@ -26,16 +39,58 @@ const Orders = (props) => {
         getData();
     }, [])
 
+    const toggleModal = (value)=>{
+        console.log(value)
+        setModalList(value)
+        setModalVisible(!modalVisible)
+    }
+
+    const renderList = ({item, index}) =>{
+        if(item)
+        {
+            return(
+                <ItemCard2 orderList = {item} index = {index} toggleModal = {toggleModal} />
+            )
+        }
+    }
+
+    const renderList2 = ({item, index})=>{
+        if (item) {
+            return (
+                <ItemCard3 pid={item.pid} sr={index + 1} pname={item.pname} qty={item.pqty}/>
+            )
+        }
+    }
 
     return (
         <View style={styles.screen}>
+            <Modal
+                visible = {modalVisible}
+                animationType="slide"
+                transparent={false}
+                style ={{flex: 1}}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                  }}
+            >
+                <Entypo.Button name="cross" iconStyle={styles.icon} backgroundColor="white" onPress={()=>{setModalVisible(!modalVisible)}} />
+                <View style={styles.header}>
+                    <Text style={[styles.border, { flex: 0.15, }]} >Sr. No.</Text>
+                    <Text style={[styles.border, { flex: 0.7, }]}>Product Name</Text>
+                    <Text style={[styles.border, { flex: 0.4 }]}>Qty.</Text>
+                </View>
+                {modalList.length !== 0 ? <FlatList
+                    data={modalList}
+                    keyExtractor={(item) => item.pid.toString()}
+                    renderItem={renderList2}
+                /> : <Text>No Items Yet</Text>}
+            </Modal>
             {
-                // orders.length === 0 ? <Text>No orders to display.</Text> :
-                // <FlatList
-                //     data={orders}
-                //     keyExtractor={(item) => item.pid.toString()}
-                //     renderItem={renderList}
-                // /> 
+                orders.length === 0 ? <Text>No orders to display.</Text> :
+                <FlatList
+                    data={orders}
+                    renderItem={renderList}
+                /> 
             }
             
         </View>
@@ -47,6 +102,27 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    border: {
+        borderWidth: 2,
+        textAlign: 'center',
+        padding: 10,
+        color: 'black',
+        backgroundColor: 'white',
+        borderColor: '#0BE881'
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 20
+    },
+    icon: {
+        marginTop: 10,
+        fontSize: 40,
+        padding: 3,
+        textAlign: 'center',
+        color: 'black',
+        flex: 1,
     }
 });
 
